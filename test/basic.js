abc0fc3cae625678b04045145a2bdc70158cb966
George Snelling <georgesn@gmail.com>
2012-02-20 19:27:01 -0800

    test tweaks

--- test/basic.js
+++ test/basic.js
@@ -5,6 +5,7 @@
 // Just say yummy to module globals in tests
 var
   assert = require('assert'),
+  util = require('util'),
   req = require('request'),
   _ = require('underscore'),
   log = require('../lib/log'),
@@ -26,50 +27,47 @@ var
     body: JSON.stringify(_body)
   }
 
-function checkCode(res, code) {
+function parse(res, code) {
+  // works if named function is called directly, but not from nodeunit
+  // var caller = arguments.callee.caller.name || 'unknown'
   code = code || 200
-  assert(res && res.statusCode && res.statusCode === code)
+  assert(res && res.statusCode && res.statusCode === code, "Bad statusCode")
+  res.body = JSON.parse(res.body)
+  assert(res.body && res.body.count === parseInt(res.body.count), "Bad body.count") // 0 is ok
+  if (res.body.data) assert(res.body.data instanceof Array, "Bad body.data")
 }
 
 log('\nTesting ' + _baseUri)
 
-exports.getUsers = function(test) {
-  req.get(_uri, function(err, res, body) {
-    checkCode(res)
-    body = JSON.parse(body)
-    assert(body && body.data && body.data instanceof Array)
+exports.getUsers = function (test) {
+  req.get(_uri, function(err, res) {
+    parse(res)
     test.done()
   })
 }
 
 // delete first in case old test left data around
-exports.delUsers = function(test) {
+exports.delUsers = function delUsers2(test) {
   req.del(_uri + '/__ids:tid', function(err, res) {
-    checkCode(res) // delete unfound record will return statusCode 200
+    parse(res)
     test.done()
   })
 }
 
 exports.addUser = function(test) {
   var options = _.clone(_options)
-  req.post(options, function(err, res, body) {
-    checkCode(res)
-    body = JSON.parse(body)
-    assert(body.count && body.count === 1)
-    assert(body && body.data && body.data instanceof Array)
-    assert(body.data.length === 1)
-    assert(body.data[0]._id && body.data[0]._id === _body.data._id)
+  req.post(options, function(err, res) {
+    parse(res)
+    assert(res.body.count === 1)
+    assert(res.body.data[0]._id && res.body.data[0]._id === _body.data._id)
     test.done()
   })
 }
 
 exports.checkUser = function(test) {
-  req.get(_uri + "/__ids:tid", function(err, res, body) {
-    checkCode(res)
-    body = JSON.parse(body)
-    assert(body && body.count && body.count === 1)
-    assert(body.data && body.data && body.data instanceof Array)
-    assert(body.data[0].name && body.data[0].name === _body.data.name)
+  req.get(_uri + "/__ids:tid", function(err, res) {
+    parse(res)
+    assert(res.body.data[0].name && res.body.data[0].name === _body.data.name)
     test.done()
   })
 }
@@ -80,20 +78,17 @@ exports.updateUser = function(test) {
   body.data.name = 'Test User2'
   options.body= JSON.stringify(body)
   options.uri = _uri + '/__ids:' + _body.data._id
-  req.post(options, function(err, res, body) {
-    checkCode(res)
-    body = JSON.parse(body)
-    assert(body && body.count && body.count === 1)
+  req.post(options, function(err, res) {
+    parse(res)
+    assert(res.body.count === 1)
     test.done()
   })
 }
 
 exports.checkUpdatedUser = function(test) {
-  req.get(_uri + '/__ids:' + _body.data._id, function(err, res, body) {
-    checkCode(res)
-    body = JSON.parse(body)
-    assert(body && body.data && body.data instanceof Array)
-    assert(body.data[0].name === 'Test User2')
+  req.get(_uri + '/__ids:' + _body.data._id, function(err, res) {
+    parse(res)
+    assert(res.body.data[0].name === 'Test User2')
     test.done()
   })
 }
