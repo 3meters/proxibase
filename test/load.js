1dba178afc9cc48cbc9d004e111ba7f55aa83794
George Snelling <georgesn@gmail.com>
2012-02-21 13:46:53 -0800

    posts now expect data inside array for symetry with get

---
+++ test/load.js
@@ -0,0 +1,45 @@
+
+
+var
+  req = require('request'),
+  _ = require('underscore'),
+  log = require('../../lib/log'),
+  parse = require('./util').parseRes,
+  _baseUri = require('../util')._baseUri,
+  _body = {
+    data: {
+      _id: "tid",
+      name: "Test User",
+      email: "foo@bar.com"
+    }
+  },
+  _options = {
+    uri: _uri,
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(_body)
+  }
+
+
+
+
+
+exports.prox = function() {
+
+   _body = {
+    data: {
+      _id: "tid001",
+      name: "Test Entity",
+      type: "picture"
+    }
+  },
+  _options = {
+    uri: _uri,
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(_body)
+  }
+
+}
