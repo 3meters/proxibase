c954242cf0c492cef58bb1413c41a3af87796a4a
Jay Massena <jay@3meters.com>
2012-03-29 22:41:13 -0700

    Added database integrity check for orphaned links and observations. Has both method and command line interface support.

---
+++ tools/admin/checkOrphans.js
@@ -0,0 +1,13 @@
+/*
+ * Perform integrity checks on the database
+ */
+
+var
+  cli = require('commander'),  // command line interface
+  checkOrphans = require('../../lib/admin/integrity').checkOrphans
+
+cli
+  .option('-d, --database <database>', 'database name [prox]', String, 'prox')
+  .parse(process.argv)
+
+checkOrphans(cli.database, null)
