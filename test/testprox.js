8f458b08bf2f357aba25b4148ed7b7fa60638d13
George Snelling <georgesn@gmail.com>
2012-03-30 14:10:09 -0700

    made tests more easily automatable. see test/readme.txt.  added runtime batch files for managing production

---
+++ test/testprox.js
@@ -0,0 +1,42 @@
+/*
+ * testprox.js: run the proxibase nodeunit tests
+ *   see https://github.com/caolan/nodeunit
+ *
+ *   useage:  node testprox
+ */
+
+process.chdir(__dirname)
+
+var
+  cli = require('commander'),
+  reporter = require('nodeunit').reporters.default, 
+  req = require('request'),
+  serverUrl = 'https://api.localhost:8044', 
+  testDir = 'tests'
+  log = require('../lib/util').log
+
+
+cli
+  .option('-s --server <url>', 'Server url [' + serverUrl + ']')
+  .option('-t --testdir <dir>', 'Test dir [' + testDir + ']')
+  .parse(process.argv)
+
+if (cli.server) serverUrl = cli.server
+if (cli.testdir) testDir = cli.testdir
+
+exports.getBaseUri = function() {
+  return serverUrl
+}
+
+log('\nTesting: ' + exports.getBaseUri())
+log('Tests: ' + testDir)
+
+// make sure the test server is running
+req.get(serverUrl, function(err, res) {
+  if (err) {
+    log('Fatal: the test server is not responding')
+    process.exit(1)
+  }
+  reporter.run([testDir])
+})
+
