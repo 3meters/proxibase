4ab5a83847aaa3d4ebfcaad1f0bc84440647f20d
George Snelling <georgesn@gmail.com>
2012-04-05 00:42:40 -0700

    tests now attempt to start test server if it is not already running

--- test/testprox.js
+++ test/testprox.js
@@ -8,15 +8,18 @@
 process.chdir(__dirname)
 
 var
+  spawn = require('child_process').spawn,
+  req = require('request'),
   cli = require('commander'),
   reporter = require('nodeunit').reporters.default,
-  req = require('request'),
   ensureDb = require('./ensureDb'),
   dbProfile = require('./constants').dbProfile,
   testDir = 'tests',
   configFile= 'configtest.js',
   config,
   serverUri,
+  testServer,
+  testServerStarted = false,
   util = require('../lib/util'),
   log = util.log
 
@@ -46,20 +49,60 @@ exports.getBaseUri = function() {
 // ensure the tests start with a clean smokeTest database
 ensureDb(dbProfile.smokeTest, function(err) {
   if (err) throw err
-  checkServer()
+  ensureServer()
 })
 
-function checkServer() {
-  log('\nTesting: ' + serverUri)
-  log('Tests: ' + testDir)
+function ensureServer() {
+
+  log('Checking for test server ' + serverUri)
 
   // make sure the test server is running
   req.get(serverUri, function(err, res) {
     if (err) {
-      log('Fatal: the test server is not responding')
-      process.exit(1)
+      // start the test server
+      log('Starting test server ' + serverUri + ' using configFile ' + configFile)
+      process.chdir(__dirname + '/..')
+      testServer = spawn('node', ['prox.js', '--config', configFile])
     }
-    reporter.run([testDir])
+    else {
+      return runTests()
+    }
+
+    testServer.stdout.setEncoding('utf8')
+    testServer.stderr.setEncoding('utf8')
+
+    testServer.stderr.on('data', function(data) {
+      util.logErr(data)
+    })
+
+    testServer.stderr.on('exit', function(code) {
+      util.logErr('Fatal: could not start test server. Code ' + code)
+      process.exit(code)
+    })
+
+    testServer.stdout.on('data', function(data) {
+      if (!testServerStarted) log(data)
+      // parse output to see if sever is ready.  fragile!
+      if (data.indexOf('listen') >= 0) {
+        testServerStarted = true
+        req.get(serverUri, function(err, res) {
+          if (err) throw err
+          return runTests()
+        })
+      }
+    })
   })
 }
 
+function runTests() {
+  log('\nTesting: ' + serverUri)
+  log('Tests: ' + testDir)
+  process.chdir(__dirname)
+  reporter.run([testDir], null, finished)
+}
+
+function finished(err) {
+  if (err) throw err
+  if (testServer) testServer.kill()
+  log('Tests finished')
+}
