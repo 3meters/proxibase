d5c506cfe8109a3eae9ea2feb152b3061b19fcc0
George Snelling <georgesn@gmail.com>
2013-02-07 20:39:23 -0800

    converted t7_clientVersion to new constructors

--- test/basic/t7_clientVersion.js
+++ test/basic/t7_clientVersion.js
@@ -2,14 +2,10 @@
  *  Proxibase client version tests
  */
 
-var assert = require('assert')
-var request = require('request')
 var util = require('utils')
 var log = util.log
 var testUtil = require('../util')
-var Req = testUtil.Req
-var check = testUtil.check
-var dump = testUtil.dump
+var t = testUtil.treq
 var constants = require('../constants')
 var dbProfile = constants.dbProfile.smokeTest
 var userCred
@@ -31,35 +27,26 @@ exports.getSessions = function (test) {
 
 // get version info and also make sure the server is responding
 exports.getVersion = function(test) {
-  var req = new Req({
-    method: 'get',
-    uri: '/client'
-  })
-  request(req, function(err, res) {
-    check(req, res)
-    assert(res.body.data.version === '0.0.0', dump(req, res))
+  t.get('/client', function(err, res, body) {
+    t.assert(body.data.version === '0.0.0')
     test.done()
   })
 }
 
 exports.setVersionRequiresAuth = function(test) {
-  var req = new Req({
+  t.post({
     uri: '/client',
-    body: {
-      data: {
+    body: { data: {
         updateUri: updateUri,
         version: '5.2.0'
-      }
-    }
-  })
-  request(req, function(err, res) {
-    check(req, res, 401) // requires admin
+      } } }, 401,
+  function(err, res, body) {
     test.done()
   })
 }
 
 exports.setVersionRequiresAdmin = function(test) {
-  var req = new Req({
+  t.post({
     uri: '/client?' + userCred,
     body: {
       data: {
@@ -67,15 +54,13 @@ exports.setVersionRequiresAdmin = function(test) {
         version: '5.2.0'
       }
     }
-  })
-  request(req, function(err, res) {
-    check(req, res, 401) // requires admin
+  }, 401, function(err, res, body) {
     test.done()
   })
 }
 
 exports.canSetVersion = function(test) {
-   var req = new Req({
+  t.post({
     uri: '/client?' + adminCred,
     body: {
       data: {
@@ -83,76 +68,45 @@ exports.canSetVersion = function(test) {
         version: '1.2.3'
       }
     }
-  })
-  request(req, function(err, res) {
-    check(req, res) // requires admin
-    var req2 = new Req({
-      method: 'get',
-      uri: '/'
-    })
-    request(req2, function(err, res2) {
-      check(req2, res2)
-      assert(res2.body.clientVersion === '1.2.3', dump(req2, res2))
+  }, function(err, res, body) {
+    t.get('/', function(err, res2, body) {
+      t.assert(body.clientVersion === '1.2.3')
       test.done()
     })
   })
 }
 
 exports.badMajorVersionFailsProperly = function(test) {
-  var req = new Req({
-    method: 'get',
-    uri: '/?version=0.9.5'
-  })
-  request(req, function(err, res) {
-    check(req, res, 400)
-    assert(res.body.error.code === 400.4, dump(req, res)) // badVersion
+  t.get('/?version=0.9.5', 400, function(err, res, body) {
+    t.assert(body.error.code === 400.4) // badVersion
     test.done()
   })
 }
 
 exports.badMinorVersionFailsProperly = function(test) {
-  var req = new Req({
-    method: 'get',
-    uri: '/?version=1.1.5'
-  })
-  request(req, function(err, res) {
-    check(req, res, 400)
-    assert(res.body.error.code === 400.4) // badVersion
+  t.get('/?version=1.1.5', 400, function(err, res, body) {
+    t.assert(body.error.code === 400.4) // badVersion
     test.done()
   })
 }
 
 exports.upgradeVersionHintWorks = function(test) {
-  var req = new Req({
-    method: 'get',
-    uri: '/?version=1.2.0'
-  })
-  request(req, function(err, res) {
-    check(req, res)  // success
-    assert(res.body.upgrade === true) //
+  t.get('/?version=1.2.0', function(err, res, body) {
+    t.assert(body.upgrade === true) //
     test.done()
   })
 }
 
 exports.currentVersionSuccedesQuietly = function(test) {
-  var req = new Req({
-    method: 'get',
-    uri: '/?version=1.2.3'
-  })
-  request(req, function(err, res) {
-    assert((typeof res.body.upgrade === 'undefined'))
+  t.get('/?version=1.2.3', function(err, res, body) {
+    t.assert(util.type.isUndefined(body.upgrade))
     test.done()
   })
 }
 
 exports.futureVersionSuccedesQuietly = function(test) {
-  var req = new Req({
-    method: 'get',
-    uri: '/?version=1.2.9'
-  })
-  request(req, function(err, res) {
-    check(req, res)  // success
-    assert((typeof res.body.upgrade === 'undefined'))
+  t.get('/?version=1.2.9', function(err, res, body) {
+    t.assert(util.type.isUndefined(body.upgrade))
     test.done()
   })
 }
@@ -163,7 +117,7 @@ exports.futureVersionSuccedesQuietly = function(test) {
 // get /client?refresh=true
 //
 exports.canRefreshVersionViaDatabaseAndRefreshParam = function(test) {
-  var req = new Req({
+  t.post({
     uri: '/data/documents/' + util.statics.clientVersion._id + '?' + adminCred,
     body: {
       data: {
@@ -173,30 +127,13 @@ exports.canRefreshVersionViaDatabaseAndRefreshParam = function(test) {
         }
       }
     }
-  })
-  request(req, function(err, res) {
-    check(req, res)
-    req = new Req({
-      method: 'get',
-      uri: '/'
-    })
-    request(req, function(err, res) {
-      check(req, res)
-      assert(res.body.clientVersion === '1.2.3', dump(req, res))  // not refreshed
-      req = new Req({
-        method: 'get',
-        uri: '/client?refresh=true'
-      })
-      request(req, function(err, res) {
-        check(req, res)
-        assert(res.body.data.version === '2.3.4', dump(req, res))
-        req = new Req({
-          method: 'get',
-          uri: '/'
-        })
-        request(req, function(err, res) {
-          check(req, res)
-          assert(res.body.clientVersion === '2.3.4', dump(req, res)) // global config var has been refreshed
+  }, function(err, res, body) {
+    t.get('/', function(err, res, body) {
+      t.assert(body.clientVersion === '1.2.3')  // not refreshed
+      t.get('/client?refresh=true', function(err, res, body) {
+        t.assert(body.data.version === '2.3.4')
+        t.get('/', function(err, res, body) {
+          t.assert(body.clientVersion === '2.3.4') // global config var has been refreshed
           test.done()
         })
       })
