19666ae48fb0aa6172899dfd44fca2123b05eedf
George Snelling <georgesn@gmail.com>
2013-06-11 10:48:59 -0700

    migrate

--- tools/migrate/9/migrate.js
+++ tools/migrate/9/migrate.js
@@ -4,11 +4,10 @@
 
 var util = require('proxutils')
 var log = util.log
-var r = require('request')
-  .defaults({
-    json: true,
-    strictSSL: false,
-  })
+var request = require('request').defaults({
+  json: true,
+  strictSSL: false,
+})
 var async = require('async')
 var assert = require('assert')
 
@@ -19,23 +18,25 @@ var oldCred = ''
 var newCred = ''
 
 var cold = {
-  actions: true,
-  devices: true,
-  users: true,
-  entities: true,
-  links: true,
-  documents: true,
-  beacons: true,
+  users: '0001',
+  entities: '0004',
+  links: '0005',
+  actions: '0006',
+  documents: '0007',
+  beacons: '0008',
+  devices: '0009'
 }
+
+var cnew = util.statics.collectionIds
 
 function run() {
   signin(function() {
-    async.eachSeries(Object.keys(cold), migrate, finish)
+    async.eachSeries(Object.keys(cold), migrateCollection, finish)
   })
 }
 
 function getCred(uri, cb) {
-  r.post({
+  request.post({
     uri: uri + '/auth/signin',
     body: {
       user: {
@@ -61,13 +62,94 @@ function signin(cb) {
   })
 }
 
-function migrate(cName, cb) {
+function migrateCollection(cName, cb) {
+  getDoc(cName, 0, cb)
+}
+
+function getDoc(cName, i, cb) {
+  var uri = oldUri + '/data/' + cName + '?sort[_id]=1&limit=1&skip=' + i + '&' + oldCred
+  request.get(uri, function(err, res, body) {
+    if (err) return cb(err)
+    var doc = body.data[0]
+    fixIds(doc, cName)
+    if (!migrate[cName]) return cb()
+    migrate[cName](doc, function(err) {
+      if (err) throw err
+      if (body.more) {
+        i++
+        getDoc(cName, i, cb) // recurse
+      }
+      else {
+        log('Migrated ' + i + ' ' + cName)
+        cb()  // done, call back
+      }
+    })
+  })
+}
+
+function fixIds(doc, cName) {
+  if (doc._id)       doc._id =       fixId(doc._id, cName)
+  if (doc._owner)    doc._owner =    fixId(doc._owner, 'users')
+  if (doc._creator)  doc._creator =  fixId(doc._creator, 'users')
+  if (doc._modifier) doc._modifier = fixId(doc._modifier, 'users')
+}
+
+function fixId(id, cName) {
+  var idParts = id.split('.')
+  idParts[0] = cnew[cName] // replace old collection id with new
+  return idParts.join('.')
+}
+
+var migrate = {}
+
+migrate.users = function(doc, cb) {
+  write('users', doc, cb)
+}
+
+migrate.entities = function(doc, cb) {
   cb()
+}
+
+migrate.actions = function(doc, cb) {
+  cb()
+}
+
+migrate.links = function(doc, cb) {
+  cb()
+}
+
+migrate.documents = function(doc, cb) {
+  cb()
+}
+
+migrate.beacons = function(doc, cb) {
+  cb()
+}
+
+migrate.devices = function(doc, cb) {
+  cb()
+}
+
+var errors = []
+function write(cName, doc, cb) {
+  request.post({
+    uri: newUri + '/data/' + cName + '?' + newCred,
+    body: {data: doc},
+  }, function(err, res, body) {
+    if (err) {
+      errors.push(doc._id)
+      util.logErr(err)
+    }
+    return cb(err)
+  })
 }
 
 function finish(err) {
   if (err) throw err
-  log('migrated ', cold)
+  log('Migrated: ', cold)
+  if (errors.length) {
+    log('Errors: ', errors)
+  }
 }
 
 run()
