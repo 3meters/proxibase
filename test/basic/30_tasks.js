5396f94e73fd34031a4852831968c2e34b8c7922
George Snelling <georgesn@gmail.com>
2013-09-08 23:51:46 -0700

    progress on scheduled tasks, mongo methods still not working

--- test/basic/30_tasks.js
+++ test/basic/30_tasks.js
@@ -7,7 +7,6 @@ var log = util.log
 var testUtil = require('../util')
 var t = testUtil.treq
 var constants = require('../constants')
-var dbProfile = constants.dbProfile.smokeTest
 var userCred
 var adminCred
 var staticVersion = util.statics.clientVersion
@@ -22,5 +21,58 @@ exports.getSessions = function (test) {
       adminCred = 'user=' + session._owner + '&session=' + session.key
       test.done()
     })
+  })
+}
+
+var sched1 = {"s": [0,1,2]}
+
+var task1 = {
+  "schedule":{
+    "schedules": [ sched1 ]
+  },
+  "module":"utils",
+  "method":"log",
+  "args": ["What follows should be an object:", {"n1":2,"s1":"foo"}]
+}
+
+var task2 = {
+  "schedule":{
+    "schedules": [ sched1 ]
+  },
+  "module":"utils",
+  "method":"db.documents.safeInsert",
+  "args": [{"type":"taskTest"}]
+}
+
+exports.usersCannotPostTasks = function(test) {
+  t.post({
+    uri: '/data/tasks?' + userCred,
+    body: { data: task1 }
+  }, 401, function(err, res, body) {
+    test.done()
+  })
+}
+
+exports.adminCanPostTasks = function(test) {
+  t.post({
+    uri: '/data/tasks?' + adminCred,
+    body: { data: task1 }
+  }, 201, function(err, res, body) {
+    test.done()
+  })
+}
+
+exports.dbCommandsWork = function(test) {
+  t.post({
+    uri: '/data/tasks?' + adminCred,
+    body: { data: task2 }
+  }, 201, function(err, res, body) {
+    setTimeout(function(){
+      t.get('/data/documents?find[type]=taskTest',
+        function(err, res, body) {
+          t.assert(3 === body.length)
+          test.done()
+        })
+    }, 4000)
   })
 }
