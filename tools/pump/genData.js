d618ddf3526d4ec9a902c426181551e4ca23bb59
George Snelling <georgesn@gmail.com>
2013-10-04 22:12:59 -0700

    major slimdown of genData

--- tools/pump/genData.js
+++ tools/pump/genData.js
@@ -1,7 +1,5 @@
 /*
- * Generate dummy data for a proxibase server
- *   Save to JSON files or directly to mongodb
- *   Silently overwrites existing files or tables
+ * Generate dummy data for a proxibase tests
  */
 
 // var util = require('proxutils') // load proxibase extentions to node util
@@ -13,11 +11,9 @@ var path = require('path')
 var async = require('async')
 var constants = require('../../test/constants')
 var testUtil = require('../../test/util')
-var tableIds = util.statics.collectionIds
-var table = {}                            // Map of tables to be generated
+var clMap = {}                            // Map of collections to be generated
 var startTime                             // Elapsed time counter
 var db                                    // Mongodb connection object
-var save                                  // Save function
 var options = {                           // Default options
   users: 3,                           // Count of users
   beacons: 3,                         // Count of beacons
@@ -26,9 +22,6 @@ var options = {                           // Default options
   cpe: 5,                             // Comments per post and place entity
   ape: 5,                             // Applinks per place
   database: 'proxTest',               // Database name
-  validate: false,                    // Validate database data against schema
-  files: false,                       // Output to JSON files rather than to datbase
-  out: 'files'                        // File output directory
 }
 var beaconIds = []
 var placeIds = []
@@ -47,43 +40,23 @@ module.exports = function(profile, callback) {
     options[key] = profile[key]
   }
 
-  if (options.files) {
-    // save to files
-    if (!path.existsSync(options.out)) fs.mkdirSync(options.out)
-    log('Saving to files...')
-    save = saveTo.file
-    run(callback)
-  }
+  var config = util.config           // Use the default server database connection
+  config.db.database = options.database     // Override database name
+  var dbUri = 'mongodb://' + config.db.host + ':' + config.db.port +  '/' + config.db.database
 
-  else {
-    // save to database
-    var config = util.config           // Use the default server database connection
-    config.db.database = options.database     // Override database name
-    var dbUri = 'mongodb://' + config.db.host + ':' + config.db.port +  '/' + config.db.database + '?safe=true'
-
-    if (options.validate) {
-      log('Saving to database ' + dbUri + ' with validation')
-      save = saveTo.dbValidate
-    }
-    else {
-      log('Saving to database ' + dbUri)
-      save = saveTo.db
-    }
-
-    mongo.connect(dbUri, function(err, database) {
-      if (err) return callback(err)
-      db = database
-      db.dropDatabase(function(err) {
+  mongo.connect(dbUri, function(err, database) {
+    if (err) return callback(err)
+    db = database
+    db.dropDatabase(function(err) {
+      if (err) throw err
+      dblib.init(config, function(err, proxdb) {
         if (err) throw err
-        dblib.init(config, function(err, proxdb) {
-          if (err) throw err
-          db.close()
-          db = proxdb
-          return run(callback)
-        })
+        db.close()
+        db = proxdb
+        return run(callback)
       })
     })
-  }
+  })
 }
 
 function run(callback) {
@@ -92,161 +65,109 @@ function run(callback) {
   genUsers()
 
   log('generating beacons')
-  genEntityRecords([0], null, options.beacons, util.statics.schemaBeacon, null)
+  genEntityRecords([0], options.beacons, 'beacons', null)
 
   log('generating places')
-  genEntityRecords(beaconIds, tableIds['beacons'], options.epb, util.statics.schemaPlace, util.statics.typeProximity)
+  genEntityRecords(beaconIds, options.epb, 'places', util.statics.typeProximity)
 
   log('generating posts')
-  genEntityRecords(placeIds, tableIds['places'], options.spe, util.statics.schemaPost, util.statics.typeContent)
+  genEntityRecords(placeIds, options.spe, 'posts', util.statics.typeContent)
 
   log('generating applinks')
-  genEntityRecords(placeIds, tableIds['places'], options.ape, util.statics.schemaApplink, util.statics.typeContent)
-  
+  genEntityRecords(placeIds, options.ape, 'applinks', util.statics.typeContent)
+
   log('generating comments')
   var placeAndPostIds = placeIds.concat(postIds)
-  genEntityRecords(placeAndPostIds, tableIds['places'], options.cpe, util.statics.schemaComment, util.statics.typeContent)
+  genEntityRecords(placeAndPostIds, options.cpe, 'comments', util.statics.typeContent)
 
   saveAll(function(err) {
-    if (err) return callback(err)
-    // if (!options.files) db.close()
-    var elapsedTime = ((new Date().getTime()) - startTime) / 1000
     if (db) db.close()
+    if (err) return callback(err)
+    var elapsedTime = ((new Date().getTime()) - startTime) / 1000
     log('genData finished in ' + elapsedTime + ' seconds')
     return callback()
   })
 }
 
 function genUsers() {
-  table['users'] = []
+  clMap['users'] = []
   for (var i = 0; i < options.users; i++) {
     var user = constants.getDefaultRecord('users')
     user._id = testUtil.genId('users', i)
     user.name = 'Test User ' + (i + 1)
     user.email = 'testuser' + (i + 1) + '@3meters.com'
     user.password = 'doobar' + i
-    table.users.push(user)
+    clMap.users.push(user)
   }
 
-  table.links = []
-  for (var i = 0; i < table.users.length; i++) {
-    for (var j = 0; j < table.users.length; j++) {
-      if (i == j) continue
-
-        // like
-        var likeLink = constants.getDefaultRecord('links')
-        likeLink._id = testUtil.genId('links', linkCount)
-        likeLink.type = util.statics.typeLike
-        likeLink._from = table.users[i]._id
-        likeLink.fromCollectionId = tableIds['users']
-        likeLink.fromSchema = 'user'
-        likeLink._to = table.users[j]._id
-        likeLink.toCollectionId = tableIds['users']
-        likeLink.toSchema = 'user'
-        likeLink._owner = table.users[i]._id
-        table['links'].push(likeLink)
-        linkCount++
-
-        // watch
-        var watchLink = constants.getDefaultRecord('links')
-        watchLink._id = testUtil.genId('links', linkCount)
-        watchLink.type = util.statics.typeWatch
-        watchLink._from = table.users[i]._id
-        watchLink.fromCollectionId = tableIds['users']
-        watchLink.fromSchema = 'user'
-        watchLink._to = table.users[j]._id
-        watchLink.toCollectionId = tableIds['users']
-        watchLink.toSchema = 'user'
-        likeLink._owner = table.users[i]._id
-        table['links'].push(watchLink)
-        linkCount++
-    }
-  }
 }
 
 function genDocuments() {
-  table['documents'] = []
-  table['documents'].push(constants.getDefaultRecord('documents'))
+  clMap['documents'] = []
+  clMap['documents'].push(constants.getDefaultRecord('documents'))
 }
 
-function genEntityRecords(parentIds, parentCollectionName, count, entitySchema, linkType) {
+function genEntityRecords(parentIds, count, collection, linkType) {
 
-  var tableName = entitySchema + "s"
-  table[tableName] = table[tableName] || []
-  table['links'] = table['links'] || []
+  clMap[collection] = clMap[collection] || []
+  clMap['links'] = clMap['links'] || []
 
   for (var p = 0; p < parentIds.length; p++) {
     for (var i = 0; i < count; i++) {
 
-      var newEnt = constants.getDefaultRecord(tableName)
+      var newEnt = constants.getDefaultRecord(collection)
 
       // Entity
-      if (entitySchema === util.statics.schemaBeacon) {
+      if ('beacons' === collection) {
         newEnt._id = testUtil.genBeaconId(i)
         newEnt.bssid = newEnt._id.substring(5)
-        newEnt.ssid = newEnt.ssid + ' ' + (entityCount[tableName] + 1)
-        newEnt._owner = util.adminUser._id
+        newEnt.ssid = newEnt.ssid + ' ' + (entityCount[collection] + 1)
       }
       else {
-        newEnt._id = testUtil.genId(tableName, entityCount[tableName])
+        newEnt._id = testUtil.genId(collection, entityCount[collection])
       }
-      newEnt.name = newEnt.name + ' ' + (entityCount[tableName] + 1)
-      newEnt._creator = newEnt._modifier = testUtil.genId('users', (entityCount[tableName] % options.users))
+      newEnt.name = newEnt.name + ' ' + (entityCount[collection] + 1)
+      newEnt._creator = newEnt._modifier = testUtil.genId('users', (entityCount[collection] % options.users))
 
-      table[tableName].push(newEnt)
-      entityCount[tableName]++
+      clMap[collection].push(newEnt)
+      entityCount[collection]++
 
-      if (entitySchema !== util.statics.schemaBeacon) {
+      if ('beacons' !== collection) {
 
         // Link
         var newLink = constants.getDefaultRecord('links')
         newLink._id = testUtil.genId('links', linkCount)
         newLink.type = linkType
         newLink._from = newEnt._id
-        newLink.fromCollectionId = tableIds[tableName]
-        newLink.fromSchema = util.statics.collectionSchemaMap[tableName]
         newLink._to = parentIds[p]
-        newLink.toCollectionId = tableIds[parentCollectionName]
-        newLink.toSchema = util.statics.collectionSchemaMap[parentCollectionName]
-        newLink._owner = newEnt._creator
 
-        if (entitySchema === util.statics.schemaComment 
-          || entitySchema === util.statics.schemaPost
-          || entitySchema === util.statics.schemaApplink) {
-          newLink.strong = true
+        switch (collection) {
+          case 'comments':
+          case 'posts':
+          case 'applinks':
+            newLink.strong = true
         }
 
-        if (entitySchema === util.statics.schemaPlace) {
+        if ('places' === collection) {
           newLink.proximity = { primary: true, signal: -80 }
-          newLink._owner = util.adminUser._id
         }
 
-        table['links'].push(newLink)
+        clMap['links'].push(newLink)
         linkCount++
 
         // Create
         var createLink = constants.getDefaultRecord('links')
         createLink._id = testUtil.genId('links', linkCount)
 
-        createLink.type = util.statics.typeCreate
+        createLink.type = 'create'
         createLink._from = newEnt._creator
-        createLink.fromCollectionId = tableIds['users']
-        createLink.fromSchema = util.statics.collectionSchemaMap['users']
-
         createLink._to = newEnt._id
-        createLink.toCollectionId = tableIds[tableName]
-        createLink.toSchema = util.statics.collectionSchemaMap[tableName]
-        createLink._owner = newEnt._creator
 
-        if (entitySchema === util.statics.schemaPlace) {
-          createLink._owner = util.adminUser._id
-        }
-
-        table['links'].push(createLink)        
+        clMap['links'].push(createLink)
         linkCount++
 
         // Like
-        if (entitySchema === util.statics.schemaPlace) {
+        if ('place' === collection) {
           for (var u = 0; u < options.users; u++) {
             if (u >= options.likes) break;
             var likeLink = constants.getDefaultRecord('links')
@@ -254,21 +175,15 @@ function genEntityRecords(parentIds, parentCollectionName, count, entitySchema, 
 
             likeLink.type = util.statics.typeLike
             likeLink._from = testUtil.genId('users', u)
-            likeLink.fromCollectionId = tableIds['users']
-            likeLink.fromSchema = util.statics.collectionSchemaMap['users']
-
             likeLink._to = newEnt._id
-            likeLink.toCollectionId = tableIds[tableName]
-            likeLink.toSchema = util.statics.collectionSchemaMap[tableName]
-            likeLink._owner = testUtil.genId('users', u)
 
-            table['links'].push(likeLink)        
+            clMap['links'].push(likeLink)
             linkCount++
           }
         }
 
         // Watch
-        if (entitySchema === util.statics.schemaPlace) {
+        if ('place' === collection) {
           for (var u = 0; u < options.users; u++) {
             if (u >= options.watch) break;
             var watchLink = constants.getDefaultRecord('links')
@@ -276,97 +191,53 @@ function genEntityRecords(parentIds, parentCollectionName, count, entitySchema, 
 
             watchLink.type = util.statics.typeWatch
             watchLink._from = testUtil.genId('users', u)
-            watchLink.fromCollectionId = tableIds['users']
-            watchLink.fromSchema = util.statics.collectionSchemaMap['users']
-
             watchLink._to = newEnt._id
-            watchLink.toCollectionId = tableIds[tableName]
-            watchLink.toSchema = util.statics.collectionSchemaMap[tableName]
-            likeLink._owner = testUtil.genId('users', u)
 
-            table['links'].push(watchLink)        
+            clMap['links'].push(watchLink)
             linkCount++
           }
         }
       }
 
-      if (entitySchema === util.statics.schemaBeacon) beaconIds.push(newEnt._id)
-      if (entitySchema === util.statics.schemaPlace) placeIds.push(newEnt._id)
-      if (entitySchema === util.statics.schemaApplink) applinkIds.push(newEnt._id)
-      if (entitySchema === util.statics.schemaPost) postIds.push(newEnt._id)
-      if (entitySchema === util.statics.schemaComment) commentIds.push(newEnt._id)
+      if ('beacons' === collection) beaconIds.push(newEnt._id)
+      if ('places' === collection) placeIds.push(newEnt._id)
+      if ('applinks' === collection) applinkIds.push(newEnt._id)
+      if ('posts' === collection) postIds.push(newEnt._id)
+      if ('comments' === collection) commentIds.push(newEnt._id)
 
     }
   }
 }
 
 function saveAll(callback) {
-  var tableNames = []
+  var collections = []
   var linkTable
-  for (name in table) {
-    name === 'links' ? linkTable = true : tableNames.push(name)
+  for (name in clMap) {
+    name === 'links' ? linkTable = true : collections.push(name)
   }
-  async.forEachSeries(tableNames, save, function(err) {
+  async.forEachSeries(collections, save, function(err) {
     if (err) return callback(err)
     if (linkTable) return save('links', callback)
     callback()
   })
 }
 
-function list(tableName, fn) {
-  log('tableName: ' + tableName)
-  log('fn: ' + fn)
-  fn()
-}
 
-var saveTo = {
+// save with schema validation
+function save(collection, callback) {
 
-  file:
-    // save to a JSON file ready to load via push
-    function (tableName, callback) {
-      var fileName = options.out + '/' + tableName + '.json'
-      fs.writeFileSync(fileName, JSON.stringify(table[tableName]))
-      log(table[tableName].length + ' ' + tableName)
-      return callback()
-    },
+  async.forEachSeries(clMap[collection], saveRow, function(err) {
+    if (err) return callback(err)
+    log(clMap[collection].length + ' ' + collection)
+    return callback()
+  })
 
-  db:
-    // save without schema validation
-    function (tableName, callback) {
-      var collection = db.collection(tableName)
-
-      // save row-at-a-time because mongo chokes saving large arrays
-      async.forEachSeries(table[tableName], saveRow, function(err) {
-        if (err) return callback(err)
-        log(table[tableName].length + ' ' + tableName)
-        return callback()
-      })
-
-      function saveRow(row, callback) {
-        collection.insert(row, {safe: true}, function(err) {
-          return callback(err)
-        })
-      }
-    },
-
-  dbValidate:
-    // save with schema validation
-    function (tableName, callback) {
-      var collection = db.collection(tableName)
-
-      async.forEachSeries(table[tableName], saveRow, function(err) {
-        if (err) return callback(err)
-        log(table[tableName].length + ' ' + tableName)
-        return callback()
-      })
-
-      function saveRow(row, callback) {
-        var user = util.adminUser
-        if (row._creator) user = {_id: row._creator, role: 'user'}
-        var options = {user: user}
-        collection.safeInsert(row, options, function(err) {
-          return callback(err)
-        })
-      }
-    }
+  function saveRow(row, callback) {
+    var user = util.adminUser
+    if (row._creator) user = {_id: row._creator, role: 'user'}
+    var options = {user: user}
+    db.collection(collection).safeInsert(row, options, function(err) {
+      return callback(err)
+    })
+  }
 }
