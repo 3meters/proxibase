7221c3d38e89814e68df2a809a4b958d6d029a00
George Snelling <georgesn@gmail.com>
2013-12-11 16:16:17 -0800

    tests for duped factual entry, fixes and tests for two-part web host names and bogus email addresses

--- test/basic/42_applinks_save.js
+++ test/basic/42_applinks_save.js
@@ -157,38 +157,99 @@ exports.refreshKaosamai = function(test) {
             })
             t.assert(yl)
 
-            cleanup(place, applinks)
+            cleanup(place, applinks, function(err) {
+              test.done()
+            })
           })
         })
       })
     })
   })
+}
 
+exports.blackBirdBakery = function(test) {
 
-  // return the db to a clean state.  twould be nice if the test harness did
-  // this automatically between test files.
-  function cleanup(place, applinks) {
+  if (disconnected) return skip(test)
+  var blackBirdId = '4aabf863f964a5204a5b20e3'
 
-    async.eachSeries(applinks, removeApplink, function(err) {
-      t.assert(!err)
-      t.delete({uri: '/data/places/' + place._id + '?' + adminCred}, function(err, res, body) {
-        t.assert(1 === body.count)
+  t.post({
+    uri: '/data/places?' + userCred,
+    body: {
+      data: {
+        name: 'Blackbird Bakery',
+        location: {
+          lng: -122.51951,
+          lat: 47.624969,
+        },
+        provider: {foursquare: blackBirdId},
+      },
+    }
+  }, 201, function(err, res, body) {
+    var place = body.data
+    t.assert(place && place._id)
+     t.post({
+      uri: '/applinks/get?' + userCred,
+      body: {
+        placeId: place._id,
+        save: true,
+        includeRaw: true,
+        log: true,
+        timeout: 20000,
+      }
+    }, function(err, res, body) {
+
+      var applinks = body.data
+      t.assert(applinks && applinks.length)
+      var raw = body.raw
+      t.assert(raw)
+      var appMap = {}
+      applinks.forEach(function(applink) {
+        appMap[applink.type] = appMap[applink.type] || 0
+        appMap[applink.type]++
+      })
+      t.assert(util.tipe.isUndefined(appMap.factual))
+      t.assert(appMap.website === 1)
+      t.assert(appMap.foursquare === 1)
+      t.assert(!appMap.email)
+      // These 4 prove that factual has duped this business
+      // and we have thrown own the factual crosswalk results
+      t.assert(!appMap.twitter)
+      t.assert(!appMap.urbanspoon)
+      t.assert(!appMap.yelp)
+      t.assert(!appMap.citygrid)
+      t.assert(appMap.googleplus === 1)
+      log('There is a dupe facebook entry for blackbird that we cannot detect so far')
+      t.assert(appMap.facebook === 1 || appMap.facebook === 2)
+      cleanup(place, applinks, function(err) {
         test.done()
       })
     })
+  })
+}
 
-    function removeApplink(applink, next) {
-      t.get('/data/links?filter[_from]=' + applink._id + '&filter[_to]=' + place._id,
-      function(err, res, body) {
-        t.assert(1 === body.data.length)
-        t.delete({uri: '/data/links/' + body.data[0]._id + '?' + adminCred}, function(err, res, body) {
+
+// return the db to a clean state.  twould be nice if the test harness did
+// this automatically between test files.
+function cleanup(place, applinks, cb) {
+
+  async.eachSeries(applinks, removeApplink, function(err) {
+    t.assert(!err)
+    t.delete({uri: '/data/places/' + place._id + '?' + adminCred}, function(err, res, body) {
+      t.assert(1 === body.count)
+      cb()
+    })
+  })
+  function removeApplink(applink, next) {
+    t.get('/data/links?filter[_from]=' + applink._id + '&filter[_to]=' + place._id,
+    function(err, res, body) {
+      t.assert(1 === body.data.length)
+      t.delete({uri: '/data/links/' + body.data[0]._id + '?' + adminCred}, function(err, res, body) {
+        t.assert(1 === body.count)
+        t.delete({uri: '/data/applinks/' + applink._id + '?' + adminCred}, function(err, res, body) {
           t.assert(1 === body.count)
-          t.delete({uri: '/data/applinks/' + applink._id + '?' + adminCred}, function(err, res, body) {
-            t.assert(1 === body.count)
-            next()
-          })
+          next()
         })
       })
-    }
+    })
   }
 }
