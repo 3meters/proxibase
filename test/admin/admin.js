c6832df7863d0babc01bbfea2534e2c52d119cb6
George Snelling <georgesn@gmail.com>
2014-05-19 17:53:35 -0700

    start on routes/admin/gclinks

--- test/admin/admin.js
+++ test/admin/admin.js
@@ -6,6 +6,7 @@ var util = require('proxutils')
 var log = util.log
 var testUtil = require('../util')
 var t = testUtil.treq
+var constants = require('../constants')
 var userSession
 var userCred
 var adminSession
@@ -25,36 +26,44 @@ exports.getUserSession = function(test) {
   })
 }
 
-exports.findOrphansAsAnnonFails = function(test) {
-  t.get('/admin/findorphans', 401,
-  function(err, res, body){
-    test.done()
-  })
-}
 
-exports.findOrphansAsUserFails = function(test) {
-  t.get('/admin/findorphans?' + userCred, 401,
-  function(err, res, body){
-    test.done()
-  })
-}
-
-exports.findOrphansAsAdminWorks = function(test) {
-  log('Fix Code and Test:')
-  return test.done()
-  t.get('/admin/findorphans?' + adminCred,
-  function(err, res, body){
-    t.assert(body.report)
-    test.done()
-  })
-}
-
-exports.validateWorks = function(test) {
+exports.validate = function(test) {
   t.get('/admin/validate?' + adminCred,
   function(err, res, body) {
     t.assert(body.results)
     t.assert(body.schemaErrors)
     t.assert(body.schemaErrors.length === 0)
+    test.done()
+  })
+}
+
+exports.gclinks = function(test) {
+  var goodLink = {
+    _id: 'li.goodlink',
+    _from: uid1,
+    _to: uid2,
+    type: 'watch',
+    _fromSchema: 'user',
+    _toSchema: 'user',
+  }
+  var badLink1 = {
+    _id: 'li.badLink1',
+    _from: uid1,
+    _to: uid2,
+    type: 'like',
+    _fromSchema: 'BOGUS_FROM_SCHEMA',
+    _toSchema: 'user',
+  }
+  var badLink2 = {
+    _id: 'li.badLink2',
+    _from: uid1,
+    _to: 'BOGUS_TO',
+    type: 'follow',
+    _fromSchema: 'user',
+    _toSchema: 'user',
+  }
+  util.db.links.insert([goodLink, badLink1, badLink2], function(err, count) {
+    t.assert(3 === count)
     test.done()
   })
 }
