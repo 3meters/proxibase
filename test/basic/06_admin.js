ba6ba8f49b0bc510031e49849af388d3fc9bb5a9
George Snelling <georgesn@gmail.com>
2014-05-30 17:37:45 -0700

    admin/gclinks, admin/gclinks/remove, admin/gcentities, admin/gcentities/remove, schemas/trash. tests in basic/06_admin

--- test/basic/06_admin.js
+++ test/basic/06_admin.js
@@ -27,6 +27,13 @@ exports.getUserSession = function(test) {
 }
 
 
+exports.onlyAdminsCanUse = function(test) {
+  t.get('/admin/validate?' + userCred, 401, function(err, res, body) {
+   test.done()
+  })
+}
+
+
 exports.validateTestData = function(test) {
   t.get('/admin/validate?' + adminCred, function(err, res, body) {
     t.assert(body.results)
@@ -127,36 +134,71 @@ exports.addSomeTestData = function(test) {
   })
 }
 
-exports.collectGarbageLinks = function(test) {
+exports.findBadLinks = function(test) {
   t.get('/admin/gclinks?' + adminCred, function(err, res, body) {
-    t.assert(body.removed)
-    t.assert(body.removed.length === 4)
-    db.collection('links').find({_id: /^li.gctest/}).count(function(err, count) {
-      t.assert(!err, err)
-      t.assert(count === 1)
+    t.assert(body.badLinks)
+    t.assert(body.badLinks.length === 4)
+    t.assert(body.count === 4)
+    t.assert(body.movedToTrash === 0)
+    test.done()
+  })
+}
+
+exports.moveBadLinksToTrash = function(test) {
+  t.get('/admin/gclinks/remove?' + adminCred, function(err, res, body) {
+    t.assert(body.badLinks)
+    t.assert(body.badLinks.length === 4)
+    t.assert(body.count === 4)
+    t.assert(body.movedToTrash === 4)
+    db.collection('links').find({_id: /^li\.gctest/}).toArray(function(err, links) {
+      assert(!err, err)
+      assert(links.length === 1, {links: links})
+      assert(links[0]._id === 'li.gctest.goodlink')  // good link survived the reaping
       db.collection('trash').find({fromSchema: 'link'}).toArray(function(err, docs) {
         t.assert(!err, err)
         t.assert(docs)
         t.assert(docs.length === 4)
         docs.forEach(function(doc) {
-          t.assert(doc.data._id.match(/^li\./))  // starts with li.
+          t.assert(doc.data._id.match(/^li\.gctest\.badLink/))  // starts with li.gctest.bad
         })
+        test.done()
       })
-      test.done()
     })
   })
 }
 
-exports.collectGarbageEnts = function(test) {
+exports.findOrphanedEnts = function(test) {
   t.get('/admin/gcentities?' + adminCred, function(err, res, body) {
     t.assert(body.orphans)
     t.assert(body.orphans.comments.length = 1)
     t.assert(body.orphans.messages.length = 1)
     t.assert(body.orphans.posts.length = 1)
     t.assert(body.orphans.applinks.length = 1)
+    t.assert(body.count === 4)
+    t.assert(body.movedToTrash === 0)
     test.done()
   })
 }
+
+exports.moveBadEntsToTrash = function(test) {
+  t.get('/admin/gcentities/remove?' + adminCred, function(err, res, body) {
+    t.assert(body.orphans)
+    t.assert(body.count === 4)
+    t.assert(body.movedToTrash === 4)
+    db.collection('messages').find({_id: /^me\.gctest/}).toArray(function(err, links) {
+      t.assert(!err, err)
+      t.assert(links.length === 0)
+      db.collection('trash').find({fromSchema: 'message'}).toArray(function(err, docs) {
+        t.assert(!err, err)
+        t.assert(docs)
+        t.assert(docs.length === 1, docs)
+        t.assert(docs[0].data._id === 'me.gctest.orphanMessage1')
+        test.done()
+      })
+    })
+  })
+}
+
 
 exports.cleanup = function(test) {
   db.collection('links').remove({_id: /^li.gctest/}, function(err, count) {
