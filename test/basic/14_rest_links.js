448d0d755bb48125b9fcf3e720487f1a1cf73126
George Snelling <georgesn@gmail.com>
2014-11-13 13:13:09 -0800

    fixed test 14 to work with new genData

--- test/basic/14_rest_links.js
+++ test/basic/14_rest_links.js
@@ -21,7 +21,7 @@ var dbProfile = testUtil.dbProfile
 var user1Id = 'us.010101.00000.555.000001'
 var user2Id = 'us.010101.00000.555.000002'
 var user3Id = 'us.010101.00000.555.000003'
-var cPatches = dbProfile.users * dbProfile.ppu
+var patch1Id = 'pa.010101.00000.555.000001'
 var cUsers = dbProfile.users
 
 
@@ -51,7 +51,6 @@ exports.findLinksFailProperlyOnBadInputs = function(test) {
 }
 
 exports.findLinksWorks = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + adminCred,
     body: {links: {to: {patches: 1}}},
@@ -61,6 +60,8 @@ exports.findLinksWorks = function(test) {
     var links = body.data.links
     t.assert(links.to)
     t.assert(links.to.patches)
+    var cWatch = 0
+    var cCreate = 0
     links.to.patches.forEach(function(link) {
       t.assert(link._id)
       t.assert(link.document)
@@ -71,14 +72,13 @@ exports.findLinksWorks = function(test) {
         case 'create': cCreate++; break
       }
     })
-    t.assert(cWatch === cPatches, cWatch)
-    t.assert(cCreate === 1)
+    t.assert(cWatch === dbProfile.ppu)
+    t.assert(cCreate === dbProfile.ppu)
     test.done()
   })
 }
 
 exports.findLinksNoDocumentsWorks = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
     body: {links: {to: {patches: 1}, noDocuments: true}},
@@ -88,7 +88,7 @@ exports.findLinksNoDocumentsWorks = function(test) {
     var links = body.data.links
     t.assert(links.to)
     t.assert(links.to.patches)
-    t.assert(links.to.patches.length === ((cPatches * 2) + 1))  // same as above
+    t.assert(links.to.patches.length === (2 * dbProfile.ppu))  // same as above
     links.to.patches.forEach(function(link) {
       t.assert(!link.document)
     })
@@ -97,7 +97,6 @@ exports.findLinksNoDocumentsWorks = function(test) {
 }
 
 exports.findLinksFieldProjectionWorks = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
     body: {links: {to: {patches: 1}, fields: {name: 1}, linkFields: {type: 1}}}
@@ -107,7 +106,7 @@ exports.findLinksFieldProjectionWorks = function(test) {
     var links = body.data.links
     t.assert(links.to)
     t.assert(links.to.patches)
-    t.assert(links.to.patches.length)
+    t.assert(links.to.patches.length === 2 * dbProfile.ppu)
     links.to.patches.forEach(function(doc) {
       t.assert(doc._id)
       t.assert(doc.type)
@@ -123,7 +122,6 @@ exports.findLinksFieldProjectionWorks = function(test) {
 }
 
 exports.findLinksLinkFilterWorks = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
     body: {links: {to: {patches: 1}, linkFilter: {type: 'watch'}}}
@@ -133,16 +131,15 @@ exports.findLinksLinkFilterWorks = function(test) {
     var links = body.data.links
     t.assert(links.to)
     t.assert(links.to.patches)
-    t.assert(cPatches === links.to.patches.length)
+    t.assert(links.to.patches.length === dbProfile.ppu)
     test.done()
   })
 }
 
 exports.findLinksLinkDocFilterWorks = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
-    body: {links: {to: {patches: 1}, filter: {namelc: 'museum of modern art 3'}}}
+    body: {links: {to: {patches: 1}, filter: {namelc: 'test patch 3'}}}
   }
   t.post(query, function(err, res, body) {
     t.assert(body.data)
@@ -150,14 +147,16 @@ exports.findLinksLinkDocFilterWorks = function(test) {
     var links = body.data.links
     t.assert(links.to)
     t.assert(links.to.patches)
-    t.assert(2 === links.to.patches.length)
+    t.assert(2 === links.to.patches.length) // watch and create
+    links.to.patches.forEach(function(link) {
+      t.assert(link.document.name.indexOf('Test Patch 3') === 0)
+    })
     test.done()
   })
 }
 
 
 exports.findLinksSortsDescendingByLinkIdByDefault = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
     body: {links: {to: {patches: 1}}}
@@ -174,35 +173,24 @@ exports.findLinksSortsDescendingByLinkIdByDefault = function(test) {
 }
 
 exports.findLinksSortWorks = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
-    body: {links: {to: {patches: 1}, linkFilter: {type: 'like'}, sort: [{_id: 1}]}}
+    body: {links: {to: {patches: 1}, linkFilter: {type: 'watch'}, sort: [{_id: 1}]}}
   }
   t.post(query, function(err, res, body) {
     var patchLinks = body.data.links.to.patches
-    var lastLinkId = lastDocId = ''
+    var lastLinkId = ''
     patchLinks.forEach(function(link) {
       t.assert(link._id > lastLinkId, {current: link, previous: lastLinkId})
-      t.assert(link.document._id > lastDocId, {current: link, previous: lastLinkId})
       lastLinkId = link._id
-      lastDocId = link.document._id
     })
-    // Watches were created in the oposite order of likes. See /tools/pump/genData.
-    // This test proves we are sorting on the link, not a property of the underlying document
-    var query = {
-      uri: '/find/users/' + user1Id + '?' + userCred,
-      body: {links: {to: {patches: 1}, linkFilter: {type: 'watch'}, sort: [{_id: 1}]}}  // watch not like
-    }
+    query.body.links.sort[0]._id = -1 // descending by link id
     t.post(query, function(err, res, body) {
       var patchLinks = body.data.links.to.patches
-      var lastLinkId = ''
-      var lastDocId = 'zzzzzzzz'
+      var lastLinkId = 'zzzzzzzz'
       patchLinks.forEach(function(link) {
-        t.assert(link._id > lastLinkId, {current: link, previous: lastLinkId})
-        t.assert(link.document._id < lastDocId, {current: link, previous: lastLinkId}) // < not >
+        t.assert(link._id < lastLinkId, {current: link, previous: lastLinkId})
         lastLinkId = link._id
-        lastDocId = link.document._id
       })
       test.done()
     })
@@ -210,7 +198,6 @@ exports.findLinksSortWorks = function(test) {
 }
 
 exports.findLinksPagingWorks = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
     body: {links: {to: {patches: 1}, limit: 5, sort: '-_id'}}
@@ -234,23 +221,22 @@ exports.findLinksPagingWorks = function(test) {
 }
 
 exports.findLinksPagingWorksWithFilter = function(test) {
-  return skip(test)
   var query = {
     uri: '/find/users/' + user1Id + '?' + userCred,
-    body: {links: {to: {patches: 1}, limit: 5, sort: '-_id'}}
+    body: {links: {to: {patches: 1}, linkFilter: {type: 'watch'}, limit: 2, sort: '-_id'}}
   }
   t.post(query, function(err, res, body) {
     var patchLinks = body.data.links.to.patches
-    t.assert(5 === patchLinks.length)
-    var lastLinkId = patchLinks[4]._id
-    var lastPatchId = patchLinks[4].document._id
+    t.assert(2 === patchLinks.length)
+    var lastLinkId = patchLinks[1]._id
+    var lastPatchId = patchLinks[1].document._id
     var query = {
       uri: '/find/users/' + user1Id + '?' + userCred,
-      body: {links: {to: {patches: 1}, limit: 5, skip: 5, sort: '-_id'}}
+      body: {links: {to: {patches: 1}, linkFilter: {type: 'watch'}, limit: 2, skip: 2, sort: '-_id'}}
     }
     t.post(query, function(err, res, body) {
       var patchLinks = body.data.links.to.patches
-      t.assert(5 === patchLinks.length)
+      t.assert(2 === patchLinks.length)
       t.assert(lastLinkId > patchLinks[0]._id)
       test.done()
     })
@@ -259,50 +245,49 @@ exports.findLinksPagingWorksWithFilter = function(test) {
 
 
 exports.findLinksCountWorks = function(test) {
-  return skip(test)
   var query = {
-    uri: '/find/messages/' + user1Id + '?' + userCred,
-    body: {links: {to: {patches: 1}, from: {users: 1}, count: true}}
+    uri: '/find/patches/' + patch1Id + '?' + userCred,
+    body: {links: {to: {places: 1, beacons: 1}, from: {users: 1, messages: 1}, count: true}}
   }
   t.post(query, function(err, res, body) {
-    t.assert(body.data.links.to.patches === ((2 * cPatches) + 1))  // everybody likes and watches and one creates
-    t.assert(body.data.links.from.users === ((cUsers * 2) - 2))  // everybody but user1 likes and watches her
+    t.assert(body.data.links)
+    var links = body.data.links
+    t.assert(links.to.places === dbProfile.ppp)
+    t.assert(links.to.beacons === dbProfile.bpp)
+    t.assert(links.from.users === 2)  // watch and create
+    t.assert(links.from.messages === dbProfile.mpp)
     test.done()
   })
 }
 
 exports.findLinksAcceptsArrays = function(test) {
-  return skip(test)
   var query = {
-    uri: '/find/users/' + user1Id + '?' + userCred,
-    body: {links: [{to: {patches: 1}}, {from: {users: 1}}]}
+    uri: '/find/patches/' + patch1Id + '?' + userCred,
+    body: {links: [{to: {places: 1}}, {from: {users: 1}}]}
   }
   t.post(query, function(err, res, body) {
     t.assert(body.data.links)
     t.assert(body.data.links.length === 2)  // nested in an array
     t.assert(body.data.links[0].to)
-    t.assert(body.data.links[0].to.patches)
-    t.assert(body.data.links[1].from.users.length)
+    t.assert(body.data.links[0].to.places)
+    t.assert(body.data.links[0].to.places.length === dbProfile.ppp)
     t.assert(!body.data.links[0].to.users)
     t.assert(body.data.links[1].from)
     t.assert(body.data.links[1].from.users)
-    t.assert(body.data.links[1].from.users.length)
-    t.assert(!body.data.links[1].from.patches)
+    t.assert(body.data.links[1].from.users.length === 2)
+    t.assert(!body.data.links[1].from.places)
     test.done()
   })
 }
 
 
 exports.findLinksFromWorksWithGetSyntax = function(test) {
-  return skip(test)
   var query = {
-    uri: '/find/users/' + user1Id + '?links[from][users]=1&links[linkFilter][type]=watch&' + userCred,
+    uri: '/find/patches/' + patch1Id + '?links[from][users]=1&links[linkFilter][type]=watch&' + userCred,
   }
   t.get(query, function(err, res, body) {
-    t.assert(body.data.links.from.users.length === (cUsers - 1)) // everybody watches user1 except user1
-    body.data.links.from.users.forEach(function(link) {
-      t.assert(link.type === 'watch')
-    })
+    t.assert(body.data.links.from.users.length === 1)  // create links filtered out
+    body.data.links.from.users[0].type === 'watch'
     test.done()
   })
 }
