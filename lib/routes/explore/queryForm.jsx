e957dd5d04e5972da2764a76dff973d78324c5f3
George Snelling <georgesn@gmail.com>
2014-12-12 15:49:54 -0800

    swapped lodash in for underscore, progress on jsx explorer

--- lib/routes/explore/queryForm.jsx
+++ lib/routes/explore/queryForm.jsx
@@ -1,38 +1,61 @@
 /** @jsx React.DOM */
 
+// Query component generated from a map
+
+
 var React = require('react')
 
+
+// Default field properties
 var defaultField = {
   key:   '',
-  name: '',
-  type: 'string',
+  name:  '',
+  type:  'string',
+  help:  '',
   input: null,  // or component
   patchholder: '',
-  help: '',
 }
 
+
+// Query form fields
 var fields = {
   collection: {},
   _id: {},
-  fields: {},
+  fields: {
+    help: 'name,address,city'},
   name: {},
-  query: {},
-  sort: {},
-  refs: {},
-  links: {},
+  query: {
+    help: '{city: {$in: ["Seattle", "Squim"]}}',
+  },
+  sort: {
+    help: 'city,-_id'
+  },
+  refs: {
+    help: 'true || name || name,modifiedDate'},
+  links: {
+    help: '{to: {patches: 1}, linkFilter: {type: "content"}}',
+  },
   skip: {},
   limit: {},
   datesToUTC: {},
   count: {},
-  countBy: {},
+  countBy: {
+    help: 'city,postalCode'
+  },
 }
 
-for (var key in fields) {
-  var field = fields[key]
-  field = _.extend(defaultField, field)
-  field.key = key
-  field.name = field.name || key
-  field.patchholder = field.patchholder || key
+
+// Flesh out field defaults
+for (var i in fields) {
+  var field = fields[i]
+  for (var j in defaultField) {
+    if (tipe.isUndefined(field[j])) {
+      field[j] = defaultField[j]
+    }
+  }
+  field.key = i
+  field.name = field.name || i
+  field.patchholder = field.patchholder || i
 }
 
 
@@ -41,9 +64,10 @@ var LeftCol = React.createClass({
     var rows = Object.keys(fields).map(function(key) {
       return <div className="row" key={key}>{key}</div>
     })
-    return <div className="col-left.pad">{rows}</div>
+    return <div className="col-left pad">{rows}</div>
   }
 })
+
 
 var CenterCol = React.createClass({
   render: function() {
@@ -55,9 +79,29 @@ var CenterCol = React.createClass({
         </div>
       )
     })
-    return <div className="col-center.pad">{rows}</div>
+    return (
+      <div className="col-center pad">
+        <form id="explore" method="post" action="/v1/explore">
+          {rows}
+          <input type="submit" name="cmdRun" value="Go" />
+        </form>
+      </div>
+    )
   }
 })
+
+
+var RightCol = React.createClass({
+  render: function() {
+    var rows = Object.keys(fields).map(function(key) {
+      var help = fields[key].help
+      help = help || ''
+      return <div className="row" key={key}>{help}</div>
+    })
+    return <div className="col-right pad">{rows}</div>
+  }
+})
+
 
 var QueryForm = React.createClass({
   render: function() {
@@ -65,6 +109,7 @@ var QueryForm = React.createClass({
       <div className="container">
       <LeftCol />
       <CenterCol />
+      <RightCol />
       </div>
     )
   }
